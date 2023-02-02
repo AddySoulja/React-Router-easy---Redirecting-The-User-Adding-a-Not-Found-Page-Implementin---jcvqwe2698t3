41900aa92dcf2dac899cf8c8f5b5c2091bb9cc63
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-02-02 19:22:33 +0530

    Build

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Index } from "./Pages/Index";
 import { Home } from "./Pages/Home";
+import { NotFound } from "./Pages/NotFound";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Index />} />
       <Route exact path="/home" element={<Home />} />
+      <Route exact path="*" element={<NotFound />} />
     </Routes>
   );
 };

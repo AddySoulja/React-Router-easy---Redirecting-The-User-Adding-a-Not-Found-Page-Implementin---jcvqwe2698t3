1f4f13a600e864bcf3af7794adfe4f1a58503b3f
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-02-02 19:19:58 +0530

    Buid

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Index } from "./Pages/Index";
+import { Home } from "./Pages/Home";
 
 export const AppRoutes = () => {
-    return (
-
-    )
-}
+  return (
+    <Routes>
+      <Route exact path="/" element={<Index />} />
+      <Route exact path="/home" element={<Home />} />
+    </Routes>
+  );
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React from "react";
 
+import { Link } from "react-router-dom";
 
-export const NavBar = () =>{
-    return(
-
-    )
-}
+export const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <Link to="/">
+            <li>Index page</li>
+          </Link>
+          <Link to="/home">
+            <li>Home page</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+};
